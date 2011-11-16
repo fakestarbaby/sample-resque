ba7fed3da00496a2b883884dddee836e8b7a23d7
fakestarbaby <fakestarbaby@gmail.com>
2011-11-16 12:24:48 +0900

    初回コミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleResque::Application.config.secret_token = '59d1d755cbb93d9f75cdb54f8af9242bbc4494a6a7207ba30195b60dfa940e7c6fccb86adddcec8405e56a9aa629103122c2f85273c51d7e7bdd56f5bb90fa56'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+SampleResque::Application.routes.draw do
+  root :to => "welcome#index"
+end
